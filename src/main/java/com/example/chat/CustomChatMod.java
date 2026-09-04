adff1710b656dee43bb60ed45da06c3af1bc5540
insoo159 <ethan1020514@gmail.com>
2026-09-04 22:31:31 +0800

    Create CustomChatMod.java

---
+++ src/main/java/com/example/chat/CustomChatMod.java
@@ -0,0 +1,31 @@
+package com.example.chat;
+
+import com.example.chat.config.ModConfig;
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import org.lwjgl.glfw.GLFW;
+
+public class CustomChatMod implements ClientModInitializer {
+    public static ModConfig CONFIG = new ModConfig();
+    private static KeyBinding configKey;
+
+    @Override
+    public void onInitializeClient() {
+        // 註冊快捷鍵開啟設定選單（預設：K 鍵）
+        configKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+                "key.customchat.open_config",
+                InputUtil.Type.KEYSYM,
+                GLFW.GLFW_KEY_K,
+                "category.customchat.title"
+        ));
+
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            while (configKey.wasPressed()) {
+                client.setScreen(com.example.chat.config.ConfigScreen.create(client.currentScreen));
+            }
+        });
+    }
+}
