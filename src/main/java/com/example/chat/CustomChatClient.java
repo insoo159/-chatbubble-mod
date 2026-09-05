66ef0d1e58b2b758a0f5dc36540360b0a5d06a0d
insoo159 <ethan1020514@gmail.com>
2026-09-05 10:46:38 +0800

    Update CustomChatClient.java

--- src/main/java/com/example/chat/CustomChatClient.java
+++ src/main/java/com/example/chat/CustomChatClient.java
@@ -6,7 +6,6 @@ import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
 public class CustomChatClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-        // 註冊 HUD 渲染事件
         HudRenderCallback.EVENT.register(BubbleChatRenderer::render);
     }
 }
